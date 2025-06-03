5117fdace63a4d0aea57bfdec46368f1e4b10dbb
MateuszSzczukiewicz <szcmat6277@gmail.com>
2025-06-03 20:54:52 +0200

    added custom theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rpg/screens/home/home.dart';
+import 'package:flutter_rpg/theme.dart';
 
 void main() {
   runApp(
-    const MaterialApp(home: Home()),
+    MaterialApp(
+      theme: primaryTheme,
+      home: Home(),
+    ),
   );
-}
-
-class Sandbox extends StatelessWidget {
-  const Sandbox({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sandbox'),
-        backgroundColor: Colors.grey,
-      ),
-      body: const Text('sandbox'),
-    );
-  }
 }
