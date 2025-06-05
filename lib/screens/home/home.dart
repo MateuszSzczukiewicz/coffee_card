a923afe67bc24a030933ef3ecb6b6de8f939da55
MateuszSzczukiewicz <szcmat6277@gmail.com>
2025-06-05 20:33:53 +0200

    added character cards

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,29 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/screens/home/character_card.dart';
+import 'package:flutter_rpg/screens/shared/styled_button.dart';
+import 'package:flutter_rpg/screens/shared/styled_text.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() =>
-      _MyWidgetState();
+  State<Home> createState() => _HomeState();
 }
 
-class _MyWidgetState
-    extends State<Home> {
+class _HomeState extends State<Home> {
+  List<String> characters = [
+    'mario',
+    'luigi',
+    'peach',
+    'toad',
+    'bowser',
+    'koopa',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Your Characters',
-        ),
+        title: const StyledTitle('Your Characters'),
         centerTitle: true,
       ),
       body: Container(
-        padding: const EdgeInsets.all(
-          16,
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: characters.length,
+                itemBuilder: (_, index) {
+                  return CharacterCard(characters[index]);
+                },
+              ),
+            ),
+            StyledButton(
+              onPressed: () {},
+              child: const StyledHeading('Create New'),
+            ),
+          ],
         ),
-        child: const Text('Home'),
       ),
     );
   }
